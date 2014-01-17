0c44485803accca2797c332b031ea87e07df2564
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2014-01-17 09:43:37 +0100

    just fixed configuration

--- src/main/java/com/metmi/mmasgis/dao/DbImpl.java
+++ src/main/java/com/metmi/mmasgis/dao/DbImpl.java
@@ -1,6 +1,11 @@
 package com.metmi.mmasgis.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,8 +14,37 @@ public class DbImpl implements DbDAO {
 
 	@Override
 	public List<DbDAO> getDatabases() {
-		// TODO Auto-generated method stub
+		String query = "show databases";
+		Connection conn = null;
+
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(query);
+			/*
+			 * ps.setInt(1, employee.getId()); ps.setString(2,
+			 * employee.getName()); ps.setInt(3, employee.getAge());
+			 * ps.executeUpdate();
+			 */
+			ps.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 		return null;
+	}
+
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
 	@Autowired
